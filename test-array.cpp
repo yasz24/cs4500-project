9121dbad5fcf8d27a1da24359276acc6e3cae43a
Chev <chev424@gmail.com>
2020-01-28 15:50:15 -0500

    main test method

--- test-array.cpp
+++ test-array.cpp
@@ -322,3 +322,28 @@ void test21() {
   t_true(l->index_of(u) == 2);
   OK("21");
 }
+
+int main() {
+  test1();
+  test2();
+  test3();
+  test4();
+  test5();
+  test6();
+  test7();
+  test8();
+  test9();
+  test10();
+  test11();
+  test12();
+  test13();
+  test14();
+  test15();
+  test16();
+  test17();
+  test18();
+  test19();
+  test20();
+  test21();
+  return 0;
+}
